9d6976d6fd73e92c0ca67e445576632f3a3de218
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-02-29 11:17:57 +0530

    added use-window-resize hook

--- src/App.js
+++ src/App.js
@@ -18,51 +18,70 @@ import FeaturedFlagsGlobalState from "./components/featured-flags/context";
 import FeatureFlags from "./components/featured-flags";
 import UseFetchHookTest from "./components/use-fetch/test";
 import UseOutsideClick from "./components/use-outside-click/test";
+import UseWindowResize from "./components/use-window-resize/test";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
       <Accordian />
+
       {/* Random-Color-Generator */}
       <RandomColor />
+
       {/* Star-Rating */}
       <StarRating noOfStars={10} />
+
       {/* Image-Slider */}
       <ImgSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
       />
+
       {/* Load More Data */}
       <LoadMoreData />
+
       {/* Tree-View / Menu UI Component / Recursive Navigation Menu  */}
       <TreeView menus={menus} />
+
       {/* QR Code Generator */}
       <QRCodeGenerator />
+
       {/* Light and Dark Mode */}
       <LightDarkMode />
+
       {/* Scroll Indicator */}
-      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
+      {/* <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} /> */}
+
       {/* Custom Tabs */}
       <TabsTest />
+
       {/* Custom Model PopUP */}
       <ModelTest />
+
       {/* Github Profile Finder */}
       <GithubProfileFinder />
+
       {/* Search Autocomplete */}
       <SearchAutocomplelte />
+
       {/* Tic Tac Toe */}
       <TicTacToe />
+
       {/* Featured Flags Implementation */}
       <FeaturedFlagsGlobalState>
         <FeatureFlags />
       </FeaturedFlagsGlobalState>
+
       {/* useFetch - custom Hook Test */}
       <UseFetchHookTest />
 
       {/* Use Outside-Click Hook Test */}
       <UseOutsideClick />
+
+      {/* use window resize hook test */}
+      <UseWindowResize />
     </div>
   );
 }
